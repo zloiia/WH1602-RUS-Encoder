fa91e58f48115282df6025c5be77e01d93bf81b7
Федин Дмитрий Александрович <d.fedin@narfu.ru>
2015-03-12 01:48:32 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WH1602_RUS_Encoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.original = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trans = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Оригинал";
+            // 
+            // original
+            // 
+            this.original.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.original.Location = new System.Drawing.Point(96, 13);
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(179, 20);
+            this.original.TabIndex = 1;
+            this.original.TextChanged += new System.EventHandler(this.original_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Для вставки";
+            // 
+            // trans
+            // 
+            this.trans.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trans.Location = new System.Drawing.Point(96, 40);
+            this.trans.Name = "trans";
+            this.trans.Size = new System.Drawing.Size(179, 20);
+            this.trans.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 75);
+            this.Controls.Add(this.trans);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.original);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(303, 114);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(303, 114);
+            this.Name = "Form1";
+            this.Text = "WH1602 RUS Trans";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox original;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox trans;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WH1602_RUS_Encoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void original_TextChanged(object sender, EventArgs e)
+        {
+            String translated = "";
+            foreach(char c in original.Text)
+            {
+                char toInsert = c;
+                switch(c)
+                {
+                    case 'Б': toInsert = '\xA0'; break;
+                    case 'Г': toInsert = '\xA1'; break;
+                    case 'Ё': toInsert = '\xA2'; break;
+                    case 'Ж': toInsert = '\xA3'; break;
+                    case 'З': toInsert = '\xA4'; break;
+                    case 'И': toInsert = '\xA5'; break;
+                    case 'Й': toInsert = '\xA6'; break;
+                    case 'Л': toInsert = '\xA7'; break;
+                    case 'П': toInsert = '\xA8'; break;
+                    case 'У': toInsert = '\xA9'; break;
+                    case 'Ф': toInsert = '\xAA'; break;
+                    case 'Ч': toInsert = '\xAB'; break;
+                    case 'Ш': toInsert = '\xAC'; break;
+                    case 'Ъ': toInsert = '\xAD'; break;
+                    case 'Ы': toInsert = '\xAE'; break;
+                    case 'Э': toInsert = '\xAF'; break;
+
+                    case 'Ю': toInsert = '\xB0'; break;
+                    case 'Я': toInsert = '\xB1'; break;
+                    case 'б': toInsert = '\xB2'; break;
+                    case 'в': toInsert = '\xB3'; break;
+                    case 'г': toInsert = '\xB4'; break;
+                    case 'ё': toInsert = '\xB5'; break;
+                    case 'ж': toInsert = '\xB6'; break;
+                    case 'з': toInsert = '\xB7'; break;
+                    case 'и': toInsert = '\xB8'; break;
+                    case 'й': toInsert = '\xB9'; break;
+                    case 'к': toInsert = '\xBA'; break;
+                    case 'л': toInsert = '\xBB'; break;
+                    case 'м': toInsert = '\xBC'; break;
+                    case 'н': toInsert = '\xBD'; break;
+                    case 'п': toInsert = '\xBE'; break;
+                    case 'т': toInsert = '\xBF'; break;
+
+                    case 'ч': toInsert = '\xC0'; break;
+                    case 'ш': toInsert = '\xC1'; break;
+                    case 'ъ': toInsert = '\xC2'; break;
+                    case 'ы': toInsert = '\xC3'; break;
+                    case 'ь': toInsert = '\xC4'; break;
+                    case 'э': toInsert = '\xC5'; break;
+                    case 'ю': toInsert = '\xC6'; break;
+                    case 'я': toInsert = '\xC7'; break;
+
+                    case 'Х': toInsert = '\xD5'; break;
+
+                    case 'Д': toInsert = '\xE0'; break;
+                    case 'Ц': toInsert = '\xE1'; break;
+                    case 'Щ': toInsert = '\xE2'; break;
+                    case 'д': toInsert = '\xE3'; break;
+                    case 'ф': toInsert = '\xE4'; break;
+                    case 'ц': toInsert = '\xE5'; break;
+                    case 'щ': toInsert = '\xE6'; break;
+
+                    case 'р': toInsert = 'p'; break;
+                    case 'А': toInsert = 'A'; break;
+                    case 'В': toInsert = 'B'; break;
+                    case 'е': toInsert = 'e'; break;
+                    case 'у': toInsert = 'y'; break;
+                    case 'х': toInsert = 'x'; break;
+                    case 'а': toInsert = 'a'; break;
+                    case 'о': toInsert = 'o'; break;
+                    case 'с': toInsert = 'c'; break;
+                }
+
+                translated += String.Format("\\x{0:X2}", Convert.ToInt32(toInsert));
+            }
+            trans.Text = translated;
+        }
+    }
+}
